5301c7c441c69f8aa063cf5f2fa7f63197cfa2ee
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-15 13:57:39 +0100

    fix: fixed serverless

--- src/pages/api/joke.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const joke = 'Нашел еврей пачку денег, а там не хватает';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ joke });
-}
