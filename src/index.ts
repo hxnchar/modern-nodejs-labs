19960bac5799a56588cfcb8b270e394f435f3670
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-10-21 13:32:49 +0100

    feat: added default logic

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import http from 'http';
+import 'dotenv/config';
+import { getMethod } from './helpers/api';
+const port = process.env.PORT || 8000;
+
+let method: string;
+
+http
+  .createServer((req: any, res: any) => {
+    switch (req.url) {
+      case '/json':
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        method = getMethod(req.method);
+        res.end(`Hello from ${method} json`);
+        break;
+      case '/form-data':
+        res.writeHead(200, { 'Content-Type': 'multipart/form-data' });
+        method = getMethod(req.method);
+        res.end(`Hello from ${method} form-data`);
+        break;
+      default:
+        res.end('This page doesn`t exist :(');
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`App is running on port ${port}`);
+  });
