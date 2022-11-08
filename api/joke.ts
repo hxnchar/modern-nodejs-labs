949da0782ec82bf5f0a395bb56720b23e3b066ed
Vladlen <ilusenkovladlen@gmail.com>
2022-11-08 18:26:01 +0200

    feat: added @vercel/node package / temporaly changed pre-commit

--- api/joke.ts
+++ api/joke.ts
@@ -1,5 +1,6 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 const joke = 'Нашел еврей пачку денег, а там не хватает';
 
-export default function handler(req: any, res: any) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({ joke });
 }
