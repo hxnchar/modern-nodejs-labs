01efd8b5c26dbb7f8c14f009ca2bd5c98ea1d4c5
Vladlen <ilusenkovladlen@gmail.com>
2022-11-09 14:10:47 +0200

    feat: added more code to VercelRouter

--- src/vercelRouter.ts
+++ src/vercelRouter.ts
@@ -1,55 +1,62 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { HTTPMethod } from './constants/methods';
 
+type Handler = (
+  req: VercelRequest,
+  res: VercelResponse,
+) => void | Promise<void>;
+
+// TODO: implement multiple handlers (Handler[])
 export class VercelRouter {
-  private routes: { [route: string]: { [method: string]: Function } } = {};
+  private routes: { [route: string]: { [method: string]: Handler } } = {};
   private basename: string;
 
   constructor(basename: string) {
     this.basename = basename;
   }
 
-  private use(method: HTTPMethod, route: string, handler: Function) {
+  private use(method: HTTPMethod, route: string, handler: Handler) {
     const fullRoute = this.basename.concat(route);
-    this.routes[fullRoute] = { method: handler };
+    this.routes[fullRoute] = { [method]: handler };
   }
 
-  public handle(req: VercelRequest, res: VercelResponse) {
-    console.log(req);
-    const { url, method } = req;
-    const handler = this.routes;
-    res.send({ req });
+  public async handle(req: VercelRequest, res: VercelResponse) {
+    // TODO: read Vercel docs and understand how to get this shit
+    const { url, method } = req.query;
+    // TODO: change this to url and method lol
+    const handler = this.routes['api/joke/'][HTTPMethod.GET];
+    await handler(req, res);
   }
 
-  public connect(route: string, handler: Function) {
+  public connect(route: string, handler: Handler) {
     this.use(HTTPMethod.CONNECT, route, handler);
   }
 
-  public delete(route: string, handler: Function) {
+  public delete(route: string, handler: Handler) {
     this.use(HTTPMethod.DELETE, route, handler);
   }
 
-  public get(route: string, handler: Function) {
+  public get(route: string, handler: Handler) {
     this.use(HTTPMethod.GET, route, handler);
   }
 
-  public head(route: string, handler: Function) {
+  public head(route: string, handler: Handler) {
     this.use(HTTPMethod.HEAD, route, handler);
   }
 
-  public patch(route: string, handler: Function) {
+  public patch(route: string, handler: Handler) {
     this.use(HTTPMethod.PATCH, route, handler);
   }
 
-  public post(route: string, handler: Function) {
+  public post(route: string, handler: Handler) {
     this.use(HTTPMethod.POST, route, handler);
   }
 
-  public put(route: string, handler: Function) {
+  public put(route: string, handler: Handler) {
     this.use(HTTPMethod.PUT, route, handler);
   }
 
-  public trace(route: string, handler: Function) {
+  public trace(route: string, handler: Handler) {
     this.use(HTTPMethod.TRACE, route, handler);
   }
 }
