cea5e54a5611df2b5228673b08ae957bfe2428ac
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-11-10 08:50:14 +0000

    fix: returned useful stuff

--- api/joke.ts
+++ api/joke.ts
@@ -3,9 +3,12 @@ import { VercelRouter } from '../src/vercelRouter';
 
 const jokeRouter = new VercelRouter('api/joke');
 
-jokeRouter.get('/', async (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).send({message: 'Joke about jews and холокост'})
-});
+//jokeRouter.get('/', async (req: VercelRequest, res: VercelResponse) => {
+//  res.status(200).send({ message: JSON.stringify('A funny joke was here but Vladlen has no почуття гумору')});
+//});
+
+jokeRouter.get('/', (req, res) => {res.send({message: 'BLKO'})});
+jokeRouter.delete('/', (req, res) => {res.send({message: 'DELETE'})});
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   jokeRouter.handle(req, res);
